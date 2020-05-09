78274d8eec71c92484afd56ffb873d8c60a452a2
Iza Witkowska <isa.witkowska@gmail.com>
2020-05-09 13:51:31 +0200

    Add booking widget

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,69 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
-  initMenu() {
+  initPages: function() {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    
+    const idFromHash = window.location.hash.replace('#/', '');
+        
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages) {
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks) {
+      link.addEventListener('click', function(event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+
+        /* Change URL hash */
+        window.location.hash = '#/' + id;
+      });
+    }
+    
+  },
+  activatePage: function(pageId) {
+    const thisApp = this;
+    /* add class active to matching page, remove from non-matching*/
+    for(let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    /* add class active to matching link, remove from non-matching*/
+    for(let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
+  initBooking: function() {
+    const thisApp = this;
+    thisApp.bookings = document.querySelector(select.containerOf.booking); //children?
+    for(let booking in thisApp.bookings) {
+      new Booking(booking);
+    }
+    
+  }, 
+  initMenu: function() {
     const thisApp = this;
     for(let productData in thisApp.data.products) {
       new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
@@ -28,7 +88,9 @@ const app = {
   },
   init: function() {
     const thisApp = this;
+    thisApp.initPages();
     thisApp.initData();
+    thisApp.initBooking();
   },
   initCart: function(){
     const thisApp = this;
---
+++ src/js/components/Booking.js
@@ -0,0 +1,35 @@
+import {select, templates} from '../settings.js';
+import AmountWidget from './AmountWidget.js';
+
+export class Booking {
+  constructor() {
+    const thisBooking = this;
+    thisBooking.render(booking);
+    thisBooking.initWidgets();
+    
+  }
+
+  render(booking) {
+    const thisBooking = this;
+
+    /* generate HTML based on template */
+    const generatedHTML = templates.bookingWidget();
+    /* generate empty object thisBoking.dom */
+    thisBooking.dom = {};
+    /* save to this object wrapper as argument*/
+    thisBooking.dom.wrapper = booking;
+    /* change wrapper into HTML from the template */
+    thisBooking.dom.wrapper.innerHTML = generatedHTML;
+    /* in property thisBooking.dom.peopleAmount save every singleelement find in wrapper passend to the selector select.booking.peopleAmount */
+    thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
+    /* the same for hoursAmount */
+    thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
+  }
+
+  initWidgets() {
+    const thisBooking = this;
+    thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+  }
+}
+export default Booking;
